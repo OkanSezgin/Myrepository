3bae69a771f2fa4eee79d516be2be97223c7751b
okan sezgin <sezginokan.62@gmail.com>
2020-10-22 22:05:54 -0400

    newdb

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = 
 {
-    "URI": "mongodb://localhost/webA2"
+    //"URI": "mongodb://localhost/webA2"
+    "URI": "mongodb+srv://Okansezgin:Dersim6292@mongodbserver.1du9y.mongodb.net/webA2?retryWrites=true&w=majority"
 }
